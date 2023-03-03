5dbd58e68968fb33db4cd5b37b6b47427539586a
LiaoYihong-1 <74876152+LiaoYihong-1@users.noreply.github.com>
2023-03-03 23:31:53 +0300

    add new graph

--- lab1/src/main/java/Test/DFSTest.java
+++ lab1/src/main/java/Test/DFSTest.java
@@ -10,23 +10,39 @@ public class DFSTest {
     @DisplayName("Test for DFS")
     @ParameterizedTest
     @ValueSource(ints = {0,1,2,3,4,5,6,7})
-    public void testGraph1(int i){
+    public void testGraph1(int i) {
         DFS dfs = new DFS(8);
-        int [][]answers = {
+        int[][] answers = {
                 {-1, 0, -1, -1, -1, -1, -1, -1},
                 {-1, -1, -1, -1, -1, -1, -1, -1},
-                {2,0,-1,-1,2,-1,4,-1},
-                {-1,-1,-1,-1,-1,-1,-1,-1},
-                {-1,-1,-1,-1,-1,-1,4,-1},
-                {-1,5,-1,5,-1,-1,-1,-1},
-                {-1,-1,-1,-1,6,-1,-1,-1},
-                {-1,5,-1,5,-1,7,-1,-1}
+                {2, 0, -1, -1, 2, -1, 4, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, 4, -1},
+                {-1, 5, -1, 5, -1, -1, -1, -1},
+                {-1, -1, -1, -1, 6, -1, -1, -1},
+                {-1, 5, -1, 5, -1, 7, -1, -1}
         };
-        int [][]map = {{1},{},{0,4,6},{},{6},{1,3},{4},{5}};
-        dfs.solve(map,i);
-        Assertions.assertArrayEquals(answers[i],dfs.getParent());
+        int[][] map = {{1}, {}, {0, 4, 6}, {}, {6}, {1, 3}, {4}, {5}};
+        dfs.solve(map, i);
+        Assertions.assertArrayEquals(answers[i], dfs.getParent());
     }
-
-
-
+    @DisplayName("Test for DFS")
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3,4,5,6,7})
+    public void testGraph2(int i) {
+        DFS dfs = new DFS(8);
+        int[][] answers = {
+                {-1, 0, -1, -1, -1, -1, 1, 6},
+                {1, -1, -1, -1, -1, -1, 1, 6},
+                {-1, -1, -1, -1, 2, -1, 4, 6},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, 4, -1, -1, -1, 4, 6},
+                {-1, -1, -1, 5, -1, -1, 7, 5},
+                {-1, -1, -1, -1, -1, -1, -1, 6},
+                {-1, -1, -1, -1, -1, -1, 7, -1}
+        };
+        int[][] map = {{1}, {0,6}, {4}, {},{2,6}, {3,7}, {7}, {6}};
+        dfs.solve(map, i);
+        Assertions.assertArrayEquals(answers[i], dfs.getParent());
+    }
 }
